1139fcb642fe3a68d257dc41e53ccec187165f46
Murilo Belletato <murilo.belletato@gmail.com>
2014-05-11 20:53:02 -0300

    commit coursera

--- plot4.R
+++ plot4.R
@@ -18,12 +18,20 @@ Data <- cbind(Data, DateTime)
 
 png(file = "plot4.png")
 
-par(mfrow=c(1, 2))
+par(mfrow=c(2, 2))
 
 plot(Data$DateTime , Data$Global_active_power, xlab="", 
      ylab="Global Active Power (kilowatts)", type="l")
 
-plot(Data$DateTime , Data$Voltage, xlab="", ylab="Voltage", type="l")
+plot(Data$DateTime , Data$Voltage, xlab="datetime", ylab="Voltage", type="l")
 
+plot(Data$DateTime, Data$Sub_metering_1, type = "n")
+points(Data$DateTime, Data$Sub_metering_1, col="black", type="l")
+points(Data$DateTime, Data$Sub_metering_2, col="red", type="l")
+points(Data$DateTime, Data$Sub_metering_3, col="blue", type="l")
+legend("topright", lwd=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+
+plot(Data$DateTime , Data$Global_reactive_power, xlab="datetime", type="l")
 
 dev.off()
