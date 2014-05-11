9d7fffcc8296c4cef7480c32097334760f7e3d76
Murilo Belletato <murilo.belletato@gmail.com>
2014-05-11 20:45:46 -0300

    commit coursera

---
+++ plot4.R
@@ -0,0 +1,29 @@
+## Coursera : Exploratory Data Analysis
+## @Author: mbelletato
+## Course Project 1
+## plot4.r
+
+Data <- read.table(file = "household_power_consumption.txt", 
+                   header=T, sep=";", na.strings="?", skip=66636, 
+                   nrows=2880, 
+                   col.names=c("Date", "Time", "Global_active_power", 
+                               "Global_reactive_power", "Voltage", 
+                               "Global_intensity", "Sub_metering_1", 
+                               "Sub_metering_2", "Sub_metering_3")
+)
+
+Data$Date <- as.Date(Data$Date, format="%d/%m/%Y")
+DateTime <- strptime(paste(Data$Date, Data$Time), "%Y-%m-%d %H:%M:%S")
+Data <- cbind(Data, DateTime)
+
+png(file = "plot4.png")
+
+par(mfrow=c(1, 2))
+
+plot(Data$DateTime , Data$Global_active_power, xlab="", 
+     ylab="Global Active Power (kilowatts)", type="l")
+
+plot(Data$DateTime , Data$Voltage, xlab="", ylab="Voltage", type="l")
+
+
+dev.off()
